05078348664a94508d08636dfa6484e4bcc69685
root <saung.myat@gmail.com>
2022-03-03 23:04:12 -0500

    commit tree a59b82f8b7c31845aeba2a85c474cb6eb0e01e13

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']; 
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return;
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name);
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
